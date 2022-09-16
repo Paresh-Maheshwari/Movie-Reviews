a4cbca3201bc74ad7cf1d98a9fa84345f7446781
Paresh-Maheshwari <pareshsurya721@gmail.com>
2022-09-16 19:40:35 +0530

    first

---
+++ movieapp/urls.py
@@ -0,0 +1,23 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from movie import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('about/',views.About,name='about'),
+    path('signup/',views.signup,name='signup'),
+    path('news/',include('news.urls')),
+    path('movie/', include('movie.urls')),
+    path('accounts/',include('accounts.urls')),
+
+
+]
+
+
+urlpatterns +=static(settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT)
